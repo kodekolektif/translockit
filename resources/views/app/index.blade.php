fbcef21960e0b902c72696cc6f42921ce4df0891
miarta <miarta@ipaymu.com>
2025-10-29 00:19:09 +0800

    :bug: Fix bug og title

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -20,16 +20,16 @@
    <meta name="keywords"
         content="{{ ($seo['tags'] ? $seo['tags'] . ', ' : '') . 'translock it, translock it consulting, software solutions, mobile applications, transportation solutions, cyber security, employee transportation software, school transportation software, freight transport system, car rental software, ai for accident claims, fleet consultancy, app development, cloud migration, cybersecurity logistics, logistics technology consulting, transport management software, fleet management solutions, mobility technology, logistics digital transformation, transport it consulting, logistics tech europe, transport software indonesia, fleet management indonesia, school bus management indonesia, custom mobile apps transportation, ai logistics solutions, cloud migration logistics, cybersecurity fleet management, car rental solution provider' }}" />
 
-    <meta name="author" content="{{ env('APP_URL') }}" />
+    {{-- <meta name="author" content="{{ env('APP_URL') }}" />
     <meta name="og:title" content="{{ $APPtitle ?? ''}} {{ isset($title) ? ' - '.$title:'' }}" />
     <meta name="og:description"
         content="{{ $seo['description'] ??'' }}" />
     <meta name="og:image" content="{{ $seo['image'] }}" />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta name="og:site_name" content="Translockit" />
-    <meta name="og:type" content="website" />
+    <meta name="og:type" content="website" /> --}}
 
-    <meta property="og:title" content="{{ $seo['title'] ?? 'Translockit' }}" />
+    <meta property="og:title" content="{{ $seo['title'] ?? $title }}" />
     <meta property="og:description" content="{{ $seo['description'] ?? '' }}" />
     <meta property="og:image" content="{{ $seo['image'] }}" />
     <meta property="og:url" content="{{ url()->current() }}" />
@@ -38,7 +38,7 @@
 
     <!-- ✅ Twitter Card tags (optional, but good practice) -->
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="{{ $seo['title'] ?? 'Translockit' }}" />
+    <meta name="twitter:title" content="{{ $seo['title'] ?? $title }}" />
     <meta name="twitter:description" content="{{ $seo['description'] ?? '' }}" />
     <meta name="twitter:image" content="{{ $seo['image'] }}" />
 
